284f29301d860c97d42972983c3a28bed5332df1
okji98 <dnflsmstltk10@gmail.com>
2025-05-26 17:52:27 +0900

    프로젝트 폴더 생성 및 변경

--- spring_boot/src/main/java/com/fairytale/fairytale/baby/BabyController.java
+++ spring_boot/src/main/java/com/fairytale/fairytale/baby/BabyController.java
@@ -1,8 +1,10 @@
 package com.fairytale.fairytale.baby;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@RequiredArgsConstructor
 public class BabyController {
     private final BabyService babyService;
 }
