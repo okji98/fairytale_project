f85d1cbdfe7cc85642ad8625ae7440e7cf49cd43
okji98 <dnflsmstltk10@gmail.com>
2025-06-02 11:48:31 +0900

    FastAPI 연결 작업중

---
+++ spring_boot/src/main/java/com/fairytale/fairytale/story/dto/StoryCreateRequest.java
@@ -0,0 +1,18 @@
+package com.fairytale.fairytale.story.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StoryCreateRequest {
+    private String theme;
+    private String voice;
+    private String imageMode;
+    private String title;
+    private Long userId; // 사용자 정보
+}
