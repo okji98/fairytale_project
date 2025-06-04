9191e6d45c05674cde39f21a7ad9f85c7a11c439
okji98 <dnflsmstltk10@gmail.com>
2025-06-04 11:48:59 +0900

    병합 충돌 해결

--- spring_boot/src/main/java/com/fairytale/fairytale/auth/dto/TokenResponse.java
+++ spring_boot/src/main/java/com/fairytale/fairytale/auth/dto/TokenResponse.java
@@ -1,23 +1,12 @@
 package com.fairytale.fairytale.auth.dto;
 
-<<<<<<< HEAD
 import lombok.*;
-import org.springframework.security.oauth2.jwt.DPoPProofContext;
-=======
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
->>>>>>> ff499d6d3234cd9769f50af99afea5d983c6a701
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-<<<<<<< HEAD
 @Builder
-=======
->>>>>>> ff499d6d3234cd9769f50af99afea5d983c6a701
 public class TokenResponse {
     private String accessToken;
     private String refreshToken;
